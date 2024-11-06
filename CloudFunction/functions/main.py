fc412edc608f945eddcfbb3a23f7132a245642f0
Jekaterina Macilevic <jekaterina.macilevic@dailypay.com>
2024-11-06 16:30:02 +0000

    Added function to get triggered when a new value is added to a database. The endTime is visible in funciton's logs

--- CloudFunction/functions/main.py
+++ CloudFunction/functions/main.py
@@ -2,15 +2,13 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app, credentials
+from firebase_functions import https_fn, firestore_fn
+# from firebase_admin import initialize_app, credentials, firestore,Event,DocumentSnapshot
+from firebase_admin import initialize_app, credentials, firestore
+from google.cloud.firestore import DocumentSnapshot
 
 initialize_app(
     credentials.ApplicationDefault(),  # Uses credentials from firebase login
-    {
-        "projectId": "dailyshift-7227e",
-        "databaseURL": "https://dshift.firebaseio.com"  # Use your Firestore URL here
-    }
 )
 
 
@@ -18,3 +16,21 @@ initialize_app(
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
     return https_fn.Response("Hello world!")
+
+
+# Firestore Trigger: Triggered when a new document is added to the `shift` collection
+@firestore_fn.on_document_created(document="shift/{shift_id}")
+def on_shift_created(event:firestore_fn.Event[DocumentSnapshot]):
+    """Triggered when a new document is added to the `shift` collection."""
+    # Get the newly created document data
+    new_value =event.data.to_dict()  # `after` gives the document after the update
+
+    # Print the new document's data to the logs
+    print("New Shift Document Added:")
+    print(new_value)
+
+    # Check if the 'duration' field exists
+    if "endTime" in new_value:
+        return f"Shift duration: {new_value["endTime"]} hours"
+    else:
+        return "No duration field found in this shift entry."
